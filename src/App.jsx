059aef59ed2b97fed5dcd5b0aa10aadf1838e6b1
Luiz Tavares <luizcarius70@gmail.com>
2023-09-27 20:26:54 -0300

    Estrutura inicial de componentes, rotas e CSS feitas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+import Login from './Components/Login/Login';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="" element />
+          <Route path="" element />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
