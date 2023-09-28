1fd2ebd112d12bcbf53fc89a30ba0c299264827d
Luiz Tavares <luizcarius70@gmail.com>
2023-09-27 22:00:01 -0300

    inicial do Header finalizada

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
-import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+import Dogs from '../Assets/dogs.svg?react';
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to="/" aria-label="Dogs - Home">
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link to="/login">Login / Criar</Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
