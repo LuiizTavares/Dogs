47cdff6c06f2f196c3b1293530ae12a76298df2f
Luiz Tavares <luizcarius70@gmail.com>
2023-10-06 00:13:22 -0300

    Página de Erro criada

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -4,8 +4,9 @@ import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
-import { UserContext } from '../../userContext';
+import { UserContext } from '../../UserContext';
 import styles from './Login.module.css';
+import NotFound from '../NotFound';
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
@@ -20,6 +21,7 @@ const Login = () => {
           <Route path="criar" element={<LoginCreate />} />
           <Route path="perdeu" element={<LoginPasswordLost />} />
           <Route path="resetar" element={<LoginPasswordReset />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </section>
