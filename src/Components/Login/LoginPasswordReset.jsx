ff1d6d5633ed075385f1f153c3d9b29b3ec4b9fc
Luiz Tavares <luizcarius70@gmail.com>
2023-10-08 21:32:20 -0300

    Adicionado Lazy e Suspense pros gráficos e ajustes finais

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -38,7 +38,7 @@ const LoginPasswordReset = () => {
   }
 
   return (
-    <div>
+    <section className="animeLeft">
       <Head title="Resete a Senha" />
       <h1 className="title">Resete a Senha</h1>
       <form onSubmit={handleSubmit}>
@@ -55,7 +55,7 @@ const LoginPasswordReset = () => {
         )}
         <Error error={error} />
       </form>
-    </div>
+    </section>
   );
 };
 
